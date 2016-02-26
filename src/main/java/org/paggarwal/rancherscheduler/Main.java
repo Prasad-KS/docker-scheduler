944474aa4a0df6c96e7c890be564f3b3dc609324
Peeyush Aggarwal <peeyush_16@yahoo.com>
2016-02-25 17:41:22 -0800

    Adding pages

--- src/main/java/org/paggarwal/rancherscheduler/Main.java
+++ src/main/java/org/paggarwal/rancherscheduler/Main.java
@@ -1,7 +1,22 @@
 package org.paggarwal.rancherscheduler;
 
+import com.google.common.io.CharStreams;
+import org.paggarwal.rancherscheduler.handlers.EmptyPayload;
+
+import java.io.InputStreamReader;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.staticFileLocation;
 /**
  * Created by paggarwal on 2/22/16.
  */
 public class Main {
+
+    public static void main(String[] args) throws Exception {
+        port(8080);
+        staticFileLocation("/public");
+        get("/", (request, response) -> CharStreams.toString(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("public/index.html"))));
+    }
 }
---
+++ src/main/resources/public/scripts/app.js
@@ -0,0 +1,65 @@
+/**
+ * Created by shekhargulati on 10/06/14.
+ */
+
+var app = angular.module('todoapp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+]);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        redirectTo: '/tasks'
+    }).when('/tasks', {
+        templateUrl: 'views/tasks.html',
+        controller: 'ListCtrl'
+    }).when('/scheduledtasks', {
+        templateUrl: 'views/scheduledtasks.html',
+        controller: 'ListCtrl'
+    }).when('/create', {
+        templateUrl: 'views/create.html',
+        controller: 'CreateCtrl'
+    }).otherwise({
+        redirectTo: '/tasks'
+    })
+});
+
+app.controller('ListCtrl', function ($scope, $http) {
+    $http.get('/api/v1/todos').success(function (data) {
+        $scope.todos = data;
+    }).error(function (data, status) {
+        console.log('Error ' + data)
+    })
+
+    $scope.todoStatusChanged = function (todo) {
+        console.log(todo);
+        $http.put('/api/v1/todos/' + todo.id, todo).success(function (data) {
+            console.log('status changed');
+        }).error(function (data, status) {
+            console.log('Error ' + data)
+        })
+    }
+});
+
+app.controller('CreateCtrl', function ($scope, $http, $location) {
+    $scope.todo = {
+        done: false
+    };
+
+    $scope.createTodo = function () {
+        console.log($scope.todo);
+        $http.post('/api/v1/todos', $scope.todo).success(function (data) {
+            $location.path('/');
+        }).error(function (data, status) {
+            console.log('Error ' + data)
+        })
+    }
+});
+
+app.controller('HeaderController', function ($scope, $http, $location) {
+    $scope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
+});
