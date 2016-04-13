d115dc22592ceedb8b61909b9cba6e9f5f00e914
Peeyush Aggarwal <peeyush_16@yahoo.com>
2016-04-12 21:42:30 -0700

    Added config for keeping executions in number of days.

--- src/main/java/org/paggarwal/dockerscheduler/jobs/ExecutionCleanupJob.java
+++ src/main/java/org/paggarwal/dockerscheduler/jobs/ExecutionCleanupJob.java
@@ -5,8 +5,10 @@ import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.inject.Inject;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by paggarwal on 3/21/16.
@@ -16,8 +18,11 @@ public class ExecutionCleanupJob implements Job {
     @Inject
     private ExecutionService executionService;
 
+    @Value("#{ systemEnvironment['EXECUTION_RETENTION_DAYS'] ?: 1  }")
+    private Integer executionRetentionInDays;
+
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        executionService.deleteTasksOlderThan(System.currentTimeMillis() - 24*60*60*1000);
+        executionService.deleteTasksOlderThan(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(executionRetentionInDays));
     }
 }
